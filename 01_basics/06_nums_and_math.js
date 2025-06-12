bfff47064f12d8325006ae2876f086b2a9a36ac9
numanhaider29 <38657663+numanhaider29@users.noreply.github.com>
2025-06-12 21:50:21 +0000

    math and num with floor and ceil

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,31 @@
+const score = 400
+const balance = new Number('4561686')
+// console.log(score);
+// console.log(balance);
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+othernumber = 123.878
+//console.log(othernumber.toPrecision(3))
+
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+// Maths
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.7));
+// console.log(Math.floor(4.8));
+// console.log(Math.min(3,4,5,6,7,8,9));
+// console.log(Math.max(3,4,5,6,7,8,9));
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min +1)) + min)
+
+
+
