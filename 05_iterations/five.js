f8dc9b0a084c269c5e1300018db03ea183c8b1a8
numanhaider29 <38657663+numanhaider29@users.noreply.github.com>
2025-07-16 04:56:47 +0000

    using filter method. Foreach doesn't return anything but for each does !

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -39,3 +39,16 @@ const mycodding = [
     }
 ]
 mycodding.forEach((item)=>{console.log(item.languagename)})
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+// const newnums = myNums.filter((num)=>{
+//  return   num>4
+// })
+const newnums = []
+
+myNums.forEach((num)=>{
+    if (num > 4) {
+        newnums.push(num)
+    }
+})
+console.log(newnums);
+
